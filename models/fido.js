4766ef9211c62c66b788ac2b43750ea74bcc3f2f
Ashley Richard <ashleymrichard@gmail.com>
2022-04-23 20:54:48 -0700

    Index Page Rendered

--- models/fido.js
+++ models/fido.js
@@ -1,12 +1,15 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
 
-const fidoSchema = new mongoose.Schema({
+const fidoSchema = new mongoose.Schema(
+  {
   name: { type: String, required: true },
   image: { type: String, required: true },
   breed: { type: String, required: true },
   renown: { type: String, required: true },
-  description: { type: String, required: true },
-})
+  description: { type: String, required: true},
+  deceased: Boolean,
+});
 
 const Fido = mongoose.model("Fido", fidoSchema)
 
--- server.js
+++ server.js
@@ -1,36 +1,48 @@
 // Dependencies
 const express = require("express");
 const mongoose = require("mongoose");
-const Fido = require("./models/fido.js")
+const Fido = require("./models/fido");
 const app = express();
 require("dotenv").config();
+// const DATABASE_URL = process.env.DATABASE_URL;
 
 // Middleware
 // Body parser middleware: give us access to req.body
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
+// app.use(bodyParser.urlencoded({ extended: false })); 
+// app.use(bodyParser.json());
 
 // Routes / Controllers
+// Index
+app.get("/fidofame", (req,res) => {
+    Fido.find({}, (error, allFidos) => {
+        res.render("index.ejs", {
+            fidos: allFidos,
+        })
+    })
+});
+
 // New
 app.get("/fidofame/new", (req, res) => {
     res.render("new.ejs")
 });
 
-// app.post("/fidofame", (req, res) => {
-//     res.send(req.body)
-// })
-
-app.post("/fidofame", (req, res) => {
-    // if (req.body.completed === "on") {
-    //   //if checked, req.body.completed is set to 'on'
-    //   req.body.completed = true
-    // } else {
-    //   //if not checked, req.body.completed is undefined
-    //   req.body.completed = false
-    // }
-    Fido.create(req.body, (error, createdFido) => {
-        res.send(createdFido)
-    });
+// Create
+app.post('/fidofame', (req, res) => {
+    req.body.deceased = !!req.body.deceased;
+    Fido.create(req.body, (err, createdFido) => {
+        if (err) {
+            console.log(err);
+            res.send(err);
+        } else {
+            res.redirect('/fidofame');
+        }
+    })
 });
+
+// app.get("/fidofame/new", (req, res) => {
+    // res.render("new.ejs")
+// });
 
 // Database Connection
 mongoose.connect(process.env.DATABASE_URL, {
@@ -41,10 +53,10 @@ mongoose.connect(process.env.DATABASE_URL, {
 // Database Connection Error/Success
 // Define callback functions for various events
 const db = mongoose.connection
-db.on("error", (err) => console.log(err.message + " is mongo not running?"))
-db.on("connected", () => console.log("mongo connected"))
-db.on("disconnected", () => console.log("mongo disconnected"))
+db.on("error", (err) => console.log(err.message + " is mongo not running?"));
+db.on("connected", () => console.log("mongo connected"));
+db.on("disconnected", () => console.log("mongo disconnected"));
 
 // Listener
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`server is listning on port: ${PORT}`));
+app.listen(PORT, () => console.log(`server is listning on port: ${PORT}`))
